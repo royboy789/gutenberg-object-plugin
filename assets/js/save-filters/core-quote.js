256188f06c82fc64b2f67a1a2f919db052e69816
Roy Sivan <roy@roysivan.com>
2018-04-23 12:46:21 -0700

    #1 - core/quote

---
+++ assets/js/save-filters/core-quote.js
@@ -0,0 +1,17 @@
+import { CleanFilter } from './CleanFilter';
+
+export class CoreQuote extends CleanFilter {
+
+  constructor() {
+    super( 'core-quote' );
+  }
+
+  hookCallback( attributes, name, innerBlocks ) {
+    for( let [index] of attributes.value.entries() ) {
+      attributes.value[index].children = wp.element.renderToString( attributes.value[index].children );
+    }
+    attributes.citation = wp.element.renderToString( attributes.citation );
+    return attributes;
+  }
+
+}
--- assets/js/save-filters/index.js
+++ assets/js/save-filters/index.js
@@ -9,6 +9,7 @@ import { CoreHeading } from './core-heading';
 import { CoreList } from './core-list';
 import { CorePreformatted } from './core-preformatted';
 import { CorePullquote } from './core-pullquote';
+import { CoreQuote } from './core-quote';
 
 export class SaveFilters {
   constructor() {
@@ -23,5 +24,6 @@ export class SaveFilters {
     new CoreList();
     new CorePreformatted();
     new CorePullquote();
+    new CoreQuote();
   }
 }
