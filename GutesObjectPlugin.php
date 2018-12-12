99cb6ab0da2469dc32504d704e0ae933bb171260
Roy Sivan <roy@roysivan.com>
2018-12-12 06:21:02 -0800

    1.6 build

--- GutesObjectPlugin.php
+++ GutesObjectPlugin.php
@@ -3,7 +3,7 @@
  * @wordpress-plugin
  * Plugin Name:       Gutenberg Object Plugin
  * Description:       Saves Gutenberg data as array into DB & adds API to access it
- * Version:           1.5.0
+ * Version:           1.6.0
  * Author:            Roy Sivan
  * Author URI:        https://roysivan.com
  * License:           GPL-3.0+
