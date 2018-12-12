69ed61a1ea877ce15fd788b0b1a15f423160d8ea
Roy Sivan <roy@roysivan.com>
2018-12-12 08:15:39 -0800

    fix for helper get_editor_blocks()

--- src/Helpers.php
+++ src/Helpers.php
@@ -10,7 +10,7 @@ function get_editor_blocks( $post_id = false ) {
 		return new WP_Error( 'no post id', __( 'get_editor_blocks requires a post ID', 'gutes-array' ) );
 	}
 
-	$API = API::init();
+	$API = new API();
 	$blocks = $API->get_editor_db( $post_id );
 	return json_decode( $blocks->gutes_array );
 
